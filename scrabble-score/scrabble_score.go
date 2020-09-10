16f26326bdee86d57352817ed4f480c07d8b1624
varunu28 <varun.u28@gmail.com>
2020-09-10 09:24:50 -0500

    Added scrabble_score.go

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,42 @@
+package scrabble
+
+import (
+	"strings"
+)
+
+// Score : Calculate scrabble score
+func Score(s string) int {
+	scoreMap := map[string]int{
+		"A": 1,
+		"E": 1,
+		"I": 1,
+		"O": 1,
+		"U": 1,
+		"L": 1,
+		"N": 1,
+		"S": 1,
+		"T": 1,
+		"R": 1,
+		"D": 2,
+		"G": 2,
+		"B": 3,
+		"C": 3,
+		"M": 3,
+		"P": 3,
+		"F": 4,
+		"H": 4,
+		"V": 4,
+		"W": 4,
+		"Y": 4,
+		"K": 5,
+		"J": 8,
+		"X": 8,
+		"Q": 10,
+		"Z": 10,
+	}
+	score := 0
+	for _, c := range s {
+		score += scoreMap[strings.ToUpper(string(c))]
+	}
+	return score
+}
