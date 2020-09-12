cb76ff992ef4e6ada61682fa03b7901e299d7a36
varunu28 <varun.u28@gmail.com>
2020-09-12 08:28:53 -0500

    Added nucleotide_count.go

---
+++ nucleotide-count/nucleotide_count.go
@@ -0,0 +1,30 @@
+package dna
+
+import (
+	"errors"
+)
+
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]int
+
+// DNA is a list of nucleotides.
+type DNA string
+
+// Counts generates a histogram of valid nucleotides in the given DNA.
+// Returns an error if d contains an invalid nucleotide.
+func (d DNA) Counts() (Histogram, error) {
+	h := Histogram{
+		'A': 0,
+		'C': 0,
+		'G': 0,
+		'T': 0,
+	}
+	for _, c := range d {
+		_, ok := h[c]
+		if !ok {
+			return h, errors.New("strand with invalid nucleotides")
+		}
+		h[c]++
+	}
+	return h, nil
+}
